d2f722881a7512db03e3dcc5832e7ee3b3185359
I Made Cakra Pustaka <imadecakra20@gmail.com>
2025-08-11 10:20:17 +0800

    chore: update response model

--- internal/model/response.go
+++ internal/model/response.go
@@ -1,19 +1,29 @@
 package model
 
 type ResponseMeta struct {
-	Limit uint `json:"limit"`
+	Limit  uint `json:"limit"`
 	Offset uint `json:"offset"`
-	Total uint `json:"page"`
+	Total  uint `json:"page"`
 }
 
 type DataResponse struct {
-	Status string `json:"status"`
-	Message string `json:"message"`
-	Data any `json:"data,omitempty"`
-	Meta *ResponseMeta `json:"meta,omitempty"`
+	Status  string        `json:"status"`
+	Message string        `json:"message"`
+	Data    any           `json:"data,omitempty"`
+	Meta    *ResponseMeta `json:"meta,omitempty"`
+  Stat    *ResponseStat `json:"stat,omitempty"`
+}
+
+type ResponseStat struct {
+  TotalApplication  int `json:"total_application"`
+  Pending           int `json:"pending"`
+  Interview         int `json:"interview"`
+  Offer             int `json:"offer"`
+  Rejected          int `json:"rejected"`
+  WithDraw          int `json:"withdraw"`
 }
 
 type ErrorResponse struct {
-	Status string `json:"status"`
+	Status  string `json:"status"`
 	Message string `json:"message"`
 }
